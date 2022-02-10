a01ba7bab57f1b8b1b15e39455d5e825d6560970
Brendon Forsgren <realpepino@gmail.com>
2022-02-09 20:47:18 -0700

    Added distortion coefficients to the projection function

--- include/projection/camera.hpp
+++ include/projection/camera.hpp
@@ -9,10 +9,14 @@ namespace projection {
 
 class Camera {
     using SE3d = SE3<double>;
+    using Vector5d = Eigen::Matrix<double, 5, 1>;
  public:
     Camera();
     Camera(double f, double ph, double pw, double w, double h, double u,
            double v);
+    Camera(double f, double ph, double pw, double w, double h, double u,
+           double v, const Vector5d &dist);
+
 
     inline Eigen::Matrix3d K() const { return K_; }
 
@@ -21,6 +25,8 @@ class Camera {
                             const SE3d &T_world_from_camera);
 
  private:
+    Eigen::Vector2d calculateDistortion(const Eigen::Vector2d &pix);
+
     double focal_length_;
     double pixel_width_;
     double pixel_height_;
@@ -30,6 +36,7 @@ class Camera {
     double center_y_;
 
     Eigen::Matrix3d K_;
+    Vector5d dist_coeff_;
 };
 
 }  // namespace projection
--- src/camera.cpp
+++ src/camera.cpp
@@ -4,7 +4,8 @@ namespace projection {
 
 Camera::Camera() : focal_length_{.015}, pixel_width_{1e-5},
                    pixel_height_{1e-5}, width_{1280}, height_{1024},
-                   center_x_{640}, center_y_{512} {
+                   center_x_{640}, center_y_{512},
+                   dist_coeff_{Vector5d::Zero()} {
     K_ << focal_length_/pixel_width_, 0, center_x_,
           0, focal_length_/pixel_height_, center_y_,
           0, 0, 1;
@@ -14,12 +15,24 @@ Camera::Camera() : focal_length_{.015}, pixel_width_{1e-5},
 Camera::Camera(double f, double ph, double pw, double w, double h, double u,
                double v) : focal_length_{f}, pixel_width_{pw},
                            pixel_height_{ph}, width_{w}, height_{h},
-                           center_x_{u}, center_y_{v} {
+                           center_x_{u}, center_y_{v},
+                           dist_coeff_{Vector5d::Zero()} {
     K_ << focal_length_/pixel_width_, 0, center_x_,
           0, focal_length_/pixel_height_, center_y_,
           0, 0, 1;
 }
 
+Camera::Camera(double f, double ph, double pw, double w, double h, double u,
+               double v, const Vector5d &dist) : focal_length_{f},
+                                                 pixel_width_{pw},
+                                                 pixel_height_{ph}, width_{w},
+                                                 height_{h},
+                                                 center_x_{u}, center_y_{v},
+                                                 dist_coeff_{dist} {
+    K_ << focal_length_/pixel_width_, 0, center_x_,
+          0, focal_length_/pixel_height_, center_y_,
+          0, 0, 1;
+}
 
 Eigen::Vector2i Camera::project(const Eigen::Ref<Eigen::Vector3d> &pt) {
     SE3d T_world_from_camera = SE3d::Identity();
@@ -30,9 +43,28 @@ Eigen::Vector2i Camera::project(const Eigen::Ref<Eigen::Vector3d> &pt,
                                 const SE3d &T_world_from_camera) {
     Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
     Eigen::Vector3d temp = K_ * I * T_world_from_camera.transp<double>(pt);
-    Eigen::Vector2i pix = (temp.head<2>() / temp(2)).cast<int>(); // vector2d or 2i
-    return pix;
+    Eigen::Vector2d pix = (temp.head<2>() / temp(2));
+    Eigen::Vector2d delta = calculateDistortion(pix);
+    return (pix+delta).cast<int>();
 }
 
+
+Eigen::Vector2d Camera::calculateDistortion(const Eigen::Vector2d &pix) {
+    Eigen::Vector2d delta{Eigen::Vector2d::Zero()};
+    Eigen::Vector2d center{center_x_, center_y_};
+    double r = (center - pix).norm();
+
+    // Radial distortion
+    delta = pix * (dist_coeff_(0)*pow(r,2) + dist_coeff_(1)*pow(r,4) +
+                        dist_coeff_(2)*pow(r, 6));
+
+    // Tangential
+    delta(0) += 2*dist_coeff_(3)*pix.prod() +
+                dist_coeff_(4)*(r*r + 2*pix(0)*pix(0));
+    delta(1) += dist_coeff_(3)*(r*r + 2*pix(1)*pix(1)) +
+                2*dist_coeff_(4)*pix.prod();
+
+    return delta;
+}
 
 }  // namespace projection
